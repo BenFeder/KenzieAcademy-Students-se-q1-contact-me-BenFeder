c7647cef01d155c3106dcd388f66bca51e5de587
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-09-07 17:26:34 +0000

    deploy: 56d259921f12ad560ecbe3283903bd313711e44d

--- code.js
+++ code.js
@@ -1,1 +1,23 @@
 // Your Code Here!
+
+function getValues() {
+  let nameItem = document.getElementById("name_input");
+  let nameValue = nameItem.value;
+
+  let emailItem = document.getElementById("email_input");
+  let emailValue = emailItem.value;
+
+  let messageItem = document.getElementById("message_input");
+  let messageValue = messageItem.value;
+
+  alert(
+    `Hello, ${nameValue}! Thank you for your message,
+    ${messageValue}, you wil reeive an email at
+    ${emailValue}.`);
+}
+
+let submitButton = document.getElementById("submit_button");
+
+submitButton.addEventListener("click", getValues) {
+    event.preventDefault();
+}
